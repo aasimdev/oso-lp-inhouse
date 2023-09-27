1e5b6bb7c8fb09829a33f4e03ba180f0b5715b98
aasimdev <aasimdev69@gmail.com>
2023-09-28 01:36:39 +0500

    updates

--- src/components/Video.js
+++ src/components/Video.js
@@ -25,14 +25,14 @@ const Video = () => {
     return (
         <div className="mt-8 md:mt-6 mx-auto max-w-[902px] rounded-lg md:rounded-2xl overflow-hidden text-center h-video relative">
 
-            <div className={`w-full h-full bg-black absolute left-0 top-0 right-0 bottom-0 overflow-hidden transition-all duration-300 ${preLoad ? "opacity-0" : "opacity-100"}`} />
+            <div className={`w-full h-full bg-black absolute left-0 top-0 right-0 bottom-0 overflow-hidden z-0 transition-all duration-300 ${preLoad ? "opacity-0" : "opacity-100"}`} />
 
-            <video playsInline preload="none" muted autoPlay loop controls={!handler} className={`aspect-auto object-cover transition-all duration-300 ${preLoad ? "opacity-100" : "opacity-0"}`} loading="lazy" ref={videoRef}>
+            <video playsInline preload="none" muted autoPlay loop controls={!handler} className={`aspect-auto object-cover transition-all duration-300 z-10 ${preLoad ? "opacity-100" : "opacity-0"}`} loading="lazy" ref={videoRef}>
                 <source src={'/assets/video/search.mp4'} type="video/mp4" />
             </video>
 
             {handler &&
-                <button className={`z-10 w-36 h-36 sm:w-[200px] sm:h-[200px] rounded-full bg-purple bg-opacity-20 flex justify-center items-center group absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 transition-all duration-300 ${preLoad ? "opacity-100" : "opacity-0"}`} onClick={videoHandler}>
+                <button className={`z-20 w-36 h-36 sm:w-[200px] sm:h-[200px] rounded-full bg-purple bg-opacity-20 flex justify-center items-center group absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 transition-all duration-300 ${preLoad ? "opacity-100" : "opacity-0"}`} onClick={videoHandler}>
                     <div className='w-20 h-20 sm:w-[120px] sm:h-[120px] rounded-full bg-purple flex justify-center items-center pl-2 sm:pl-3 transition-all duration-300 group-hover:bg-[#811BFF] group-focus:bg-[#9C4BFF]'>
                         <svg width="36" height="46" viewBox="0 0 36 46" fill="none" xmlns="http://www.w3.org/2000/svg" className='w-6 sm:w-9 sm:h-[46px]'>
                             <path d="M35.5 23L0.5 45.5V0.5L35.5 23Z" fill="white" />
--- src/components/loader/index.jsx
+++ src/components/loader/index.jsx
@@ -6,10 +6,10 @@ const Loader = () => {
   return (
     <div className="bg-white h-screen flex flex-col space-y-12 justify-center items-center">
       <Image src={"/assets/logo/oso.png"} alt="logo" width={93} height={128} />
-      <div class="loading-dots">
-        <div class="dot dot1"></div>
-        <div class="dot dot2"></div>
-        <div class="dot dot3"></div>
+      <div className="loading-dots">
+        <div className="dot dot1"></div>
+        <div className="dot dot2"></div>
+        <div className="dot dot3"></div>
       </div>
     </div>
   );

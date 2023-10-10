d43230942501d1dcc7188fc6ad9931d885b9eb7c
ahtisham101 <ahtisham_101@outlook.com>
2023-10-10 15:42:03 +0500

    wip

--- src/app/(landing)/components/SeenOn.jsx
+++ src/app/(landing)/components/SeenOn.jsx
@@ -19,11 +19,30 @@ function SeenOn({}) {
           className={
             "bg-gradient-to-b from-[#ffffff00] to-[#8CDCD0] hover:from-[#ffffff00] hover:to-[#58C2B1]"
           }
+          learnMore={
+            <div className='flex justify-end  items-center mt-4 '>
+              <p className='pr-4 cursor-pointer'>Lean more</p>
+              <svg
+                className='cursor-pointer'
+                width='33'
+                height='32'
+                viewBox='0 0 33 32'
+                fill='none'
+                xmlns='http://www.w3.org/2000/svg'
+              >
+                <circle cx='16.5' cy='16' r='16' fill='white' />
+                <path
+                  d='M9.38911 16.8891L20.2069 16.8891L15.238 21.858L16.5002 23.1113L23.6113 16.0002L16.5002 8.88911L15.2469 10.1424L20.2069 15.1113L9.38911 15.1113L9.38911 16.8891Z'
+                  fill='black'
+                />
+              </svg>
+            </div>
+          }
         />
         <SeenOnCard className={"gradient-seen-on-gray"} />
       </div>
       {/* chat */}
-      <div className='bg-gradient-to-b from-[#D0FAD2] to-[#ABFFAF] hover:from-[#D0FAD2] hover:to-[#7EF684] mt-[56px] p-[64px] h-[571px]'>
+      <div className='bg-gradient-to-b from-[#D0FAD2] to-[#ABFFAF] hover:from-[#D0FAD2] hover:to-[#7EF684] mt-[56px] p-[64px] h-[571px] rounded-2xl'>
         <div className='flex justify-between items-start'>
           <div>
             <p className='text-5xl font-bold'>Chat</p>
@@ -84,6 +103,71 @@ function SeenOn({}) {
                 assess leadership.{" "}
               </p>
             </div>
+          </div>
+        </div>
+      </div>
+      {/* feed */}
+
+      <div className='bg-gradient-to-b from-[#FEF5CC] to-[#FEF4CC] hover:from-[#FEF5CC] hover:to-[#FEF4CC] mt-[56px] p-[64px] h-[571px] rounded-2xl'>
+        <div className='flex justify-between items-center'>
+          <div>
+            <p className='text-5xl font-bold'>Chat</p>
+          </div>
+          <div>
+            <div className='flex justify-end items-center cursor-pointer'>
+              <p className='pr-4'>Lean more</p>
+              <svg
+                width='33'
+                height='32'
+                viewBox='0 0 33 32'
+                fill='none'
+                xmlns='http://www.w3.org/2000/svg'
+              >
+                <circle cx='16.5' cy='16' r='16' fill='white' />
+                <path
+                  d='M9.38911 16.8891L20.2069 16.8891L15.238 21.858L16.5002 23.1113L23.6113 16.0002L16.5002 8.88911L15.2469 10.1424L20.2069 15.1113L9.38911 15.1113L9.38911 16.8891Z'
+                  fill='black'
+                />
+              </svg>
+            </div>
+          </div>
+        </div>
+        <div className='flex gap-[60px]'>
+          <div className=''>
+            <p className='text-2xl font-light mt-2'>
+              <strong className='text-2xl font-medium'>Summarize</strong>
+              your custom news, then inquire with specific questions
+            </p>
+
+            <div className='flex items-start gap-2 mt-11'>
+              <Image
+                src='/assets/icons/oso-logo-black.svg'
+                alt='Example SVG'
+                width={24}
+                height={34}
+              />
+              <div className='w-[617px] rounded-[20px] bg-black'>
+                <p className='p-4 text-xl font-normal text-white'>
+                  📝 Report on Apple's iPhone 15 Camera Improvements and
+                  Flagship Features
+                  <br />
+                  <br />
+                  📸 Smart Camera Improvements in iPhone 15 The iPhone 15
+                  introduces several smart camera improvements. - Larger main
+                  sensor for better image quality and low-light performance.
+                  <br />
+                  ...
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className='mt-[36px]'>
+            <Image
+              src='/assets/images/feed-phone.png'
+              alt='Example SVG'
+              width={316.8}
+              height={653.895}
+            />
           </div>
         </div>
       </div>
--- src/app/(landing)/components/SeenOnCard.jsx
+++ src/app/(landing)/components/SeenOnCard.jsx
@@ -1,8 +1,8 @@
 import React from "react"
 import Typewriter from "typewriter-effect"
-export const SeenOnCard = ({ className }) => {
+export const SeenOnCard = ({ className, learnMore }) => {
   return (
-    <div className={`${className} w-[532px] h-[571px] px-16`}>
+    <div className={`${className} w-[532px] h-[571px] px-16 rounded-2xl`}>
       <h2 className='text-[32px] mt-16 '>OSO Search</h2>
       <p className='text-2xl font-light'>
         Delivers a{" "}
@@ -12,28 +12,13 @@ export const SeenOnCard = ({ className }) => {
         <Typewriter
           options={{
             strings: ["Where is the best sushi restaurant in Prague? 🍣"],
-            autoStart: true,
-            loop: true,
+            autoStart: false,
+            loop: false,
+            deleteSpeed: 0,
           }}
         />
       </div>
-      <div className='flex justify-end  items-center mt-4 '>
-        <p className='pr-4 cursor-pointer'>Lean more</p>
-        <svg
-          className='cursor-pointer'
-          width='33'
-          height='32'
-          viewBox='0 0 33 32'
-          fill='none'
-          xmlns='http://www.w3.org/2000/svg'
-        >
-          <circle cx='16.5' cy='16' r='16' fill='white' />
-          <path
-            d='M9.38911 16.8891L20.2069 16.8891L15.238 21.858L16.5002 23.1113L23.6113 16.0002L16.5002 8.88911L15.2469 10.1424L20.2069 15.1113L9.38911 15.1113L9.38911 16.8891Z'
-            fill='black'
-          />
-        </svg>
-      </div>
+      {learnMore}
     </div>
   )
 }

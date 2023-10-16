10a165a0214c8fd265e48b57afc8995a90dc9621
ahtisham101 <ahtisham_101@outlook.com>
2023-10-16 19:08:03 +0500

    fix: code spliting and component structure changinh

--- src/components/common/Comparisons/InnerSections/InnerSections.jsx
+++ src/components/common/Comparisons/InnerSections/InnerSections.jsx
@@ -3,14 +3,30 @@ import gsap from "gsap"
 import ScrollTrigger from "gsap/dist/ScrollTrigger"
 import { useMediaQuery } from "react-responsive"
 import { Slide } from "react-slideshow-image"
-import { FreeMode, Navigation, Pagination } from "swiper/modules";
+import { FreeMode, Navigation, Pagination } from "swiper/modules"
 import "react-slideshow-image/dist/styles.css"
 import NewsLetter from "@/components/news-letter"
-import Phone from "./Phone"
-import ComparisonContent from "./ComparisonContent"
-import { Swiper, SwiperSlide } from "swiper/react";
+import { Swiper, SwiperSlide } from "swiper/react"
 
-const InnerSection = () => {
+import ComparisonContent from "../ComparisonContent/ComparisonContent"
+import Phone from "../Phone/Phone"
+
+const InnerSections = ({
+  titleOne,
+  descriptionOne,
+  titleTwo,
+  descriptionTwo,
+  titleThree,
+  descriptionThree,
+  oso,
+  chatgpt,
+  phoneoneOso,
+  phoneOneChatGPT,
+  phoneTwoOso,
+  phoneTwoChatGPT,
+  phoneThreeOso,
+  phoneThreeChatGPT,
+}) => {
   const container = useRef()
   const isDesktop = useMediaQuery({ query: "(min-width: 720px)" })
   const [isMobile, setIsMobile] = useState(false)
@@ -110,20 +126,20 @@ const InnerSection = () => {
     <div
       id='animation-container'
       className='flex flex-col md:justify-center top-0 bg-white pt-8 md:pt-24'
-    //   ref={container}
+      //   ref={container}
     >
       <div className='md:flex md:flex-nowrap flex-wrap justify-between'>
         <div className='contentWrap w-full md:w-[346px] md:mt-[52px] flex flex-col gap-12 md:gap-9 flex-grow-0 flex-shrink-0 basis-auto mb-12 sm:mb-0'>
           {/* Stay Updated */}
           <div
-            className={`text-block opacity-100 cursor-pointer ${divVisibility.stay ? "md:opacity-100" : "md:opacity-20"
-              }`}
+            className={`text-block opacity-100 cursor-pointer ${
+              divVisibility.stay ? "md:opacity-100" : "md:opacity-20"
+            }`}
             onClick={() => toggleHandler("stay")}
           >
             <ComparisonContent
-              title=' Stay Updated'
-              description='Get instant news updates like the recent Maui Fires. Watch OSO
-          gather real-time information from diverse sources.'
+              title={titleOne}
+              description={descriptionOne}
               isMobile={isMobile}
             />
           </div>
@@ -138,34 +154,32 @@ const InnerSection = () => {
                   clickable: true,
                 }}
                 modules={[Pagination]}
-                className="!z-auto mobSwiper"
+                className='!z-auto mobSwiper'
               >
-                <SwiperSlide className="!h-auto">
+                <SwiperSlide className='!h-auto'>
                   <div className='each-slide-effect'>
                     <Phone title='OSO' obj='869795820' />
                   </div>
                 </SwiperSlide>
-                <SwiperSlide className="!h-auto">
+                <SwiperSlide className='!h-auto'>
                   <div className='each-slide-effect'>
                     <Phone title='ChatGPT' obj='869795832' />
                   </div>
                 </SwiperSlide>
               </Swiper>
-
             </div>
           )}
 
-
-
           {/* Plan your event */}
           <div
-            className={`text-block cursor-pointer ${divVisibility.plan ? "md:opacity-100" : "md:opacity-20"
-              }`}
+            className={`text-block cursor-pointer ${
+              divVisibility.plan ? "md:opacity-100" : "md:opacity-20"
+            }`}
             onClick={() => toggleHandler("plan")}
           >
             <ComparisonContent
-              title='Plan Your Events'
-              description='Wondering about the Grand Prix in Las Vegas? From dates to ticket pricing and seat recommendations, see OSO fetch it all.'
+              title={titleTwo}
+              description={descriptionTwo}
               isMobile={isMobile}
             />
           </div>
@@ -181,14 +195,14 @@ const InnerSection = () => {
                   clickable: true,
                 }}
                 modules={[Pagination]}
-                className="!z-auto mobSwiper"
+                className='!z-auto mobSwiper'
               >
-                <SwiperSlide className="!h-auto">
+                <SwiperSlide className='!h-auto'>
                   <div className='each-slide-effect'>
                     <Phone title='OSO' obj='869795741' />
                   </div>
                 </SwiperSlide>
-                <SwiperSlide className="!h-auto">
+                <SwiperSlide className='!h-auto'>
                   <div className='each-slide-effect'>
                     <Phone title='ChatGPT' obj='869795758' />
                   </div>
@@ -199,14 +213,14 @@ const InnerSection = () => {
 
           {!isMobile && (
             <div
-              className={`text-block cursor-pointer ${divVisibility.deep ? "md:opacity-100" : "md:opacity-20"
-                }`}
+              className={`text-block cursor-pointer ${
+                divVisibility.deep ? "md:opacity-100" : "md:opacity-20"
+              }`}
               onClick={() => toggleHandler("deep")}
             >
               <ComparisonContent
-                title='Deep Dive Reviews'
-                description='Looking for the best restaurant experience? Observe how OSO
-           evaluates atmosphere, culinary highlights, and more'
+                title={titleThree}
+                description={descriptionThree}
                 isMobile={isMobile}
               />
             </div>
@@ -218,25 +232,28 @@ const InnerSection = () => {
           <div className='right-elemetns relative overflow-hidden w-full flex '>
             <div className='w-full h-full relative overflow-hidden'>
               <div
-                className={`phoneWrap flex flex-nowrap md:justify-end 0 md:mt-0 mt-16 absolute left-0 top-0 right-0 bottom-0 opacity-0 transition-opacity duration-300 ease-in-out ${divVisibility.stay ? "opacity-100" : ""
-                  }`}
+                className={`phoneWrap flex flex-nowrap md:justify-end 0 md:mt-0 mt-16 absolute left-0 top-0 right-0 bottom-0 opacity-0 transition-opacity duration-300 ease-in-out ${
+                  divVisibility.stay ? "opacity-100" : ""
+                }`}
               >
-                <Phone title='OSO' obj='869795820' />
-                <Phone title='ChatGPT' obj='869795832' />
+                <Phone title={oso} obj={phoneoneOso} />
+                <Phone title={chatgpt} obj={phoneOneChatGPT} />
               </div>
               <div
-                className={`phoneWrap flex flex-nowrap md:justify-end 0 md:mt-0 mt-16 absolute left-0 top-0 right-0 bottom-0 opacity-0 transition-opacity duration-300 ease-in-out ${divVisibility.plan ? "opacity-100" : ""
-                  }`}
+                className={`phoneWrap flex flex-nowrap md:justify-end 0 md:mt-0 mt-16 absolute left-0 top-0 right-0 bottom-0 opacity-0 transition-opacity duration-300 ease-in-out ${
+                  divVisibility.plan ? "opacity-100" : ""
+                }`}
               >
-                <Phone title='OSO' obj='869795741' />
-                <Phone title='ChatGPT' obj='869795758' />
+                <Phone title={oso} obj={phoneTwoOso} />
+                <Phone title={chatgpt} obj={phoneTwoChatGPT} />
               </div>
               <div
-                className={`phoneWrap flex flex-nowrap md:justify-end 0 md:mt-0 mt-16 absolute left-0 top-0 right-0 bottom-0 opacity-0 transition-opacity duration-300 ease-in-out ${divVisibility.deep ? "opacity-100" : ""
-                  }`}
+                className={`phoneWrap flex flex-nowrap md:justify-end 0 md:mt-0 mt-16 absolute left-0 top-0 right-0 bottom-0 opacity-0 transition-opacity duration-300 ease-in-out ${
+                  divVisibility.deep ? "opacity-100" : ""
+                }`}
               >
-                <Phone title='OSO' obj='869795782' />
-                <Phone title='ChatGPT' obj='869795803' />
+                <Phone title={oso} obj={phoneThreeOso} />
+                <Phone title={chatgpt} obj={phoneThreeChatGPT} />
               </div>
             </div>
           </div>
@@ -246,9 +263,8 @@ const InnerSection = () => {
         {!isDesktop && (
           <>
             <ComparisonContent
-              title='Deep Dive Reviews'
-              description='Looking for the best restaurant experience? Observe how OSO
-      evaluates atmosphere, culinary highlights, and more'
+              title={titleOne}
+              description={descriptionOne}
               isMobile={isMobile}
             />
             <div className=' sm:mx-0 mt-12 sm:mt-0'>
@@ -259,14 +275,14 @@ const InnerSection = () => {
                   clickable: true,
                 }}
                 modules={[Pagination]}
-                className="!z-auto mobSwiper"
+                className='!z-auto mobSwiper'
               >
-                <SwiperSlide className="!h-auto">
+                <SwiperSlide className='!h-auto'>
                   <div className='each-slide-effect'>
                     <Phone title='OSO' obj='869795782' />
                   </div>
                 </SwiperSlide>
-                <SwiperSlide className="!h-auto">
+                <SwiperSlide className='!h-auto'>
                   <div className='each-slide-effect'>
                     <Phone title='ChatGPT' obj='869795803' />
                   </div>
@@ -276,8 +292,7 @@ const InnerSection = () => {
           </>
         )}
       </div>
-    </div >
+    </div>
   )
 }
-
-export default InnerSection
+export default InnerSections
--- src/container/chat/Index.js
+++ src/container/chat/Index.js
@@ -1,29 +1,31 @@
 "use client"
+import React, { useEffect, useRef, useState } from "react"
 import AdvantageCard from "@/components/advantages/AdvantageCard"
 import Advantages from "@/components/advantages/Index"
 import Banner from "@/components/common/banner/Index"
 import Testimonials from "@/components/testimonials/Index"
-import React from "react"
-
 import webImage from "../../../public/assets/images/updated.png"
 import SecondPhone from "../../../public/assets/images/mobilephone.png"
 import UpdatePhone from "../../../public/assets/images/updatePhones.png"
 import UpdatePhone2 from "../../../public/assets/images/updatephone-2.png"
 
 import FAQ from "@/components/FAQ/Index"
-import Comparison from "@/components/search/comparison/Index"
+import InnerSections from "@/components/common/Comparisons/InnerSections/InnerSections"
+import Comparisons from "@/components/common/Comparisons/Comparisons"
 
 const ChatView = () => {
   return (
     <>
-      <Banner title='OSO Chat' description='Chat uncensored about anything.' />
+      <Banner
+        title='Uncensored, intelligent AI Chat'
+        description='OSO Chat sets a new standard for AI chatbots.'
+      />
 
       <Testimonials />
-
       <Advantages title='Advantages of OSO Chat'>
         <AdvantageCard
-          title='Up to Date'
-          description="Unlike AI chatbots that have a set knowledge cutoff, <span class='font-medium'>OSO’s AI Search ensures you receive the most recent</span> and relevant information."
+          title='Uncensored'
+          description=" OSO believes in free speech and  <span class='font-medium'>unrestricted access to knowledge,</span> ensuring you receive in-depth and unfiltered answers."
           borderLess={true}
           image={webImage}
           imageWidth='532'
@@ -31,9 +33,10 @@ const ChatView = () => {
           imageHeight='427'
           color='egg-blue'
         />
+
         <AdvantageCard
-          title='Uncensored'
-          description="OSO believes in <span class='font-medium'>free speech</span> and <span class='font-medium'>unrestricted access to knowledge,</span> ensuring you receive in-depth and unfiltered answers."
+          title='Unbiased'
+          description="OSO believes in <span class='font-medium'>allowing you to formulate your own opinions</span> and  and provides different viewpoints of sensitive topics."
           borderLess={false}
           image={UpdatePhone}
           image2={SecondPhone}
@@ -41,9 +44,10 @@ const ChatView = () => {
           imageHeight='507'
           color='electric-blue'
         />
+
         <AdvantageCard
-          title='Secure'
-          description="With OSO, there's <span class='font-medium'>no need to venture into potentially harmful sites</span> since the searching is done for you."
+          title='Personalized'
+          description="OSO <span class='font-medium'> learns from your preferences</span> over time to give you answers in the format, tone, and style that suites you best."
           borderLess={false}
           image={UpdatePhone2}
           imageWidth='357'
@@ -52,8 +56,30 @@ const ChatView = () => {
         />
       </Advantages>
 
-      <Comparison />
-
+      <Comparisons title={"How OSO Chat transforms AI"}>
+        <InnerSections
+          titleOne={"Stay Updated"}
+          descriptionOne={
+            "Get instant news updates like the recent Maui Fires. Watch OSO gather real-time information from diverse sources."
+          }
+          titleTwo={"Plan Your Events"}
+          descriptionTwo={
+            "Wondering about the Grand Prix in Las Vegas? From dates to ticket pricing and seat recommendations, see OSO fetch it all."
+          }
+          titleThree={"Deep Dive Reviews"}
+          descriptionThree={
+            "Looking for the best restaurant experience? Observe how OSO evaluates atmosphere, culinary highlights, and more"
+          }
+          oso='OSO'
+          chatgpt='ChatGPT'
+          phoneoneOso={"869795820"}
+          phoneOneChatGPT={"869795832"}
+          phoneTwoOso={"869795741"}
+          phoneTwoChatGPT={"869795758"}
+          phoneThreeOso={"869795782"}
+          phoneThreeChatGPT={"869795803"}
+        />
+      </Comparisons>
       <FAQ />
     </>
   )

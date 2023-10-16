9f92b4ed1a8a952b2468258786ed1f8bf5afd022
ahtisham101 <ahtisham_101@outlook.com>
2023-10-16 23:13:46 +0500

    fix chnage

--- src/app/(landing)/components/SeenOn.jsx
+++ src/app/(landing)/components/SeenOn.jsx
@@ -71,100 +71,101 @@ function SeenOn({}) {
         <SearchAndOtherForLaptop />
         <Chat />
         <Feed />
-      </div>
-      <div className='md:flex justify-center items-center pt-8 md:pt-[60px]'>
-        <NewsLetter arrowIcon label='Join Waitlist' />
-      </div>
-      <h2 className='text-[40px] leading-tight md:text-5xl md:leading-relaxed font-bold text-black text-center mt-24'>
-        Many people already feel the magic
-      </h2>
-      <div className='pb-8 mt-24 tSlider relative'>
-        <Swiper
-          spaceBetween={0}
-          slidesPerView={"auto"}
-          navigation={{
-            nextEl: ".swiper-button-next",
-            prevEl: ".swiper-button-prev",
-          }}
-          pagination={{
-            clickable: true,
-          }}
-          freeMode={true}
-          modules={[FreeMode, Navigation, Pagination]}
-          grabCursor={true}
-          slidesOffsetBefore={slidesOffsetBefore}
-          breakpoints={{
-            0: {
-              slidesPerView: 1,
-            },
-            768: {
-              slidesPerView: 1.3,
-              pagination: false,
-            },
-            1020: {
-              slidesPerView: 1.9,
-            },
-            1200: {
-              slidesPerView: 2.2,
-            },
-            1400: {
-              slidesPerView: 2.45,
-            },
-            1537: {
-              slidesPerView: 3.2,
-            },
-            1921: {
-              slidesPerView: 4.3,
-            },
-          }}
-          className='!z-auto'
-        >
-          {isDesktop && (
-            <>
-              <div className='swiper-button-next md:mr-24 !w-12 !h-12 rounded-full bg-gray-40 p-3 transition-all duration-300 hover:bg-gray-50 focus:bg-gray-30'>
-                <svg
-                  width='24'
-                  height='24'
-                  viewBox='0 0 24 24'
-                  fill='none'
-                  xmlns='http://www.w3.org/2000/svg'
-                >
-                  <path
-                    d='M12 4L10.59 5.41L16.17 11H4V13H16.17L10.59 18.59L12 20L20 12L12 4Z'
-                    fill='black'
+
+        <div className='md:flex justify-center items-center pt-8 md:pt-[60px]'>
+          <NewsLetter arrowIcon label='Join Waitlist' />
+        </div>
+        <h2 className='text-[40px] leading-tight md:text-5xl md:leading-relaxed font-bold text-black text-center mt-24'>
+          Many people already feel the magic
+        </h2>
+        <div className='pb-8 mt-24 tSlider relative'>
+          <Swiper
+            spaceBetween={0}
+            slidesPerView={"auto"}
+            navigation={{
+              nextEl: ".swiper-button-next",
+              prevEl: ".swiper-button-prev",
+            }}
+            pagination={{
+              clickable: true,
+            }}
+            freeMode={true}
+            modules={[FreeMode, Navigation, Pagination]}
+            grabCursor={true}
+            slidesOffsetBefore={slidesOffsetBefore}
+            breakpoints={{
+              0: {
+                slidesPerView: 1,
+              },
+              768: {
+                slidesPerView: 1.3,
+                pagination: false,
+              },
+              1020: {
+                slidesPerView: 1.9,
+              },
+              1200: {
+                slidesPerView: 2.2,
+              },
+              1400: {
+                slidesPerView: 2.45,
+              },
+              1537: {
+                slidesPerView: 3.2,
+              },
+              1921: {
+                slidesPerView: 4.3,
+              },
+            }}
+            className='!z-auto'
+          >
+            {isDesktop && (
+              <>
+                <div className='swiper-button-next md:mr-24 !w-12 !h-12 rounded-full bg-gray-40 p-3 transition-all duration-300 hover:bg-gray-50 focus:bg-gray-30'>
+                  <svg
+                    width='24'
+                    height='24'
+                    viewBox='0 0 24 24'
+                    fill='none'
+                    xmlns='http://www.w3.org/2000/svg'
+                  >
+                    <path
+                      d='M12 4L10.59 5.41L16.17 11H4V13H16.17L10.59 18.59L12 20L20 12L12 4Z'
+                      fill='black'
+                    />
+                  </svg>
+                </div>
+                <div className='swiper-button-prev md:ml-24 !w-12 !h-12 rounded-full bg-gray-40 p-3 transition-all duration-300 hover:bg-gray-50 focus:bg-gray-30'>
+                  <svg
+                    width='24'
+                    height='24'
+                    viewBox='0 0 24 24'
+                    className='rotate-180'
+                    fill='none'
+                    xmlns='http://www.w3.org/2000/svg'
+                  >
+                    <path
+                      d='M12 4L10.59 5.41L16.17 11H4V13H16.17L10.59 18.59L12 20L20 12L12 4Z'
+                      fill='black'
+                    />
+                  </svg>
+                </div>
+              </>
+            )}
+            {testimonialData &&
+              testimonialData.map((item, index) => (
+                <SwiperSlide key={index} className='!h-auto'>
+                  <TestimonialItem
+                    name={item.name}
+                    description={item.description}
+                    image={item.image}
                   />
-                </svg>
-              </div>
-              <div className='swiper-button-prev md:ml-24 !w-12 !h-12 rounded-full bg-gray-40 p-3 transition-all duration-300 hover:bg-gray-50 focus:bg-gray-30'>
-                <svg
-                  width='24'
-                  height='24'
-                  viewBox='0 0 24 24'
-                  className='rotate-180'
-                  fill='none'
-                  xmlns='http://www.w3.org/2000/svg'
-                >
-                  <path
-                    d='M12 4L10.59 5.41L16.17 11H4V13H16.17L10.59 18.59L12 20L20 12L12 4Z'
-                    fill='black'
-                  />
-                </svg>
-              </div>
-            </>
-          )}
-          {testimonialData &&
-            testimonialData.map((item, index) => (
-              <SwiperSlide key={index} className='!h-auto'>
-                <TestimonialItem
-                  name={item.name}
-                  description={item.description}
-                  image={item.image}
-                />
-              </SwiperSlide>
-            ))}
-        </Swiper>
+                </SwiperSlide>
+              ))}
+          </Swiper>
+        </div>
+        <FAQ />
       </div>
-      <FAQ />
     </>
   )
 }

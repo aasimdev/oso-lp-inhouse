dba01bfc9c132d4344d3a0199f0ad60d6547bba8
hamza <hamz.4you@gmail.com>
2023-11-13 17:35:15 +0500

    update code thankyou page and checkout API

--- src/app/api/stripe-checkout-data/route.js
+++ src/app/api/stripe-checkout-data/route.js
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+const stripe = require("stripe")("sk_test_zHVJsPqk1dU1TwofOCqBxwyT00lzpPRsCb");
 
 export async function POST(req) {
   const { checkoutSessionId } = await req.json();
@@ -8,10 +8,39 @@ export async function POST(req) {
       const session = await stripe.checkout.sessions.retrieve(
         checkoutSessionId
       );
+
+      // console.log(
+      //   "----------------em",
+      //   session,
+      //   session?.customer_details?.email,
+      //   session.subscription
+      // );
+
+      const subscriptionID = session?.subscription;
+      let isMonthly = null;
+
+      if (subscriptionID) {
+        const subscription = await stripe.subscriptions.retrieve(
+          subscriptionID
+        );
+
+        const billingInterval =
+          subscription?.items?.data[0]?.price?.recurring?.interval;
+
+        // console.log("----------------em2", billingInterval);
+
+        if (billingInterval === "month") {
+          isMonthly = true;
+        }
+        if (billingInterval === "year") {
+          isMonthly = false;
+        }
+      }
+
       return NextResponse.json(
         {
           status: "success",
-          data: { customerEmail: session?.customer_details?.email },
+          data: { customerEmail: session?.customer_details?.email, isMonthly },
         },
         { status: 200 }
       );

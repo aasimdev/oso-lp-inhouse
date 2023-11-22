0fa6e95c4f175927e3df902ba422685dcb10ba28
hamza <hamz.4you@gmail.com>
2023-11-22 15:09:23 +0500

    fix

--- src/app/api/create-contact/route.js
+++ src/app/api/create-contact/route.js
@@ -70,7 +70,7 @@ export async function POST(req) {
     );
     const contactRes = await res.json();
 
-    if (res.status === 200 && email) {
+    if (email) {
       await stripe.customers.create({
         email,
         metadata: {

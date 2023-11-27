e0fd4dce76985013f786fd5ba5326b43598a6687
hamza <hamz.4you@gmail.com>
2023-11-27 21:55:18 +0500

    update videos

--- src/components/usecase/AllUsecases.js
+++ src/components/usecase/AllUsecases.js
@@ -1,12 +1,65 @@
-import React, { useMemo } from "react";
+import React, { useMemo, useEffect, useRef, useState } from "react";
 import TikTokEmbed from "./TikTokEmbed";
 
 const AllUsecases = ({ videoData }) => {
+  const containerRefs = useRef([]);
+  const [loadedVideos, setLoadedVideos] = useState([]);
+
+  console.log("--------loaded videos", loadedVideos);
+
   const memoizedTikTokEmbedComponents = useMemo(() => {
-    return videoData.map((v, i) => (
-      <TikTokEmbed key={i} videoId={v.videoId} category={v.category} />
-    ));
-  }, [videoData]);
+    return videoData.map((v, i) => {
+      containerRefs.current[i] = containerRefs.current[i] || React.createRef();
+      return (
+        <div key={i} ref={containerRefs.current[i]}>
+          {loadedVideos.includes(i) && (
+            <div key={i} ref={containerRefs.current[i]}>
+              {loadedVideos.includes(i) && (
+                <TikTokEmbed videoId={v.videoId} category={v.category} />
+              )}
+            </div>
+          )}
+        </div>
+      );
+    });
+  }, [videoData, loadedVideos, containerRefs]);
+
+  useEffect(() => {
+    const options = {
+      root: null,
+      rootMargin: "0px",
+      threshold: 0.5,
+    };
+
+    const handleIntersection = (index) => (entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting && !loadedVideos.includes(index)) {
+          console.log(`Video at index ${index} is in view`);
+          console.log("--------loaded videos inside methode", loadedVideos);
+          if (loadedVideos.length <= videoData.length) {
+            setLoadedVideos((prev) => [...prev, index]);
+          }
+        }
+      });
+    };
+
+    containerRefs.current.forEach((containerRef, index) => {
+      const observer = new IntersectionObserver(
+        handleIntersection(index),
+        options
+      );
+
+      if (containerRef.current) {
+        observer.observe(containerRef.current);
+      }
+
+      return () => {
+        if (containerRef.current) {
+          observer.unobserve(containerRef.current);
+        }
+      };
+    });
+  }, [containerRefs, loadedVideos]);
 
   return (
     <section className="py-6 sm:py-8 mx-auto max-w-6xl">
--- src/components/usecase/TikTokEmbed.js
+++ src/components/usecase/TikTokEmbed.js
@@ -2,25 +2,27 @@ import { useState } from "react";
 import PlaceholderLoading from "react-placeholder-loading";
 
 const TikTokEmbed = ({ videoId }) => {
-  // const [isPlay, setIsPlay] = useState(false);
+  const [isPlay, setIsPlay] = useState(false);
   const [isLoading, setIsLoading] = useState(true);
 
-  // const handleHover = () => {
-  //   setIsPlay(true);
-  //   setTimeout(() => {
-  //     setIsPlay(false);
-  //   }, 1000);
-  // };
+  const handleHover = () => {
+    // console.log("------hover--");
+    setIsPlay(true);
+    setTimeout(() => {
+      setIsPlay(false);
+    }, 45000);
+  };
 
   const onLoadVideo = () => {
     setIsLoading(false);
   };
 
+  // console.log("------hover--value", isPlay);
+
   return (
     <div
-      className="overflow-hidden md:mt-6 left-0 w-full h-[720px] relative"
-      // onMouseEnter={handleHover}
-      // onMouseLeave={handleMouseLeave}
+      className="overflow-hidden md:mt-6 left-0 w-full h-[600px] relative"
+      onMouseEnter={handleHover}
     >
       {isLoading && (
         <PlaceholderLoading shape="rect" width={320} height={600} />
@@ -29,7 +31,9 @@ const TikTokEmbed = ({ videoId }) => {
         className={`border border-gray-50 left-0 w-full h-full absolute ${
           isLoading ? "hidden" : ""
         }`}
-        src={`https://www.tiktok.com/embed/v2/${videoId}?lang=en&autoplay=0`}
+        src={`https://www.youtube.com/embed/${videoId}?lang=en&autoplay=${
+          isPlay ? 1 : 0
+        }&&mute=${isPlay ? 1 : 0}`}
         width="100%"
         // height="700"
         allowFullScreen

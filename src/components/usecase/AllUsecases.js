d12ef2f7e31eb771e126cab1f15b4428b56b3289
hamza <hamz.4you@gmail.com>
2023-12-12 21:16:02 +0500

    update code

--- src/components/usecase/AllUsecases.js
+++ src/components/usecase/AllUsecases.js
@@ -1,69 +1,8 @@
-import React, { useMemo, useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import UseCaseCard from "../usecaseCard";
 
 const AllUsecases = ({ videoData }) => {
-  const containerRefs = useRef([]);
-  const [loadedVideos, setLoadedVideos] = useState([]);
   const [videosToShow, setVideosToShow] = useState(9);
-
-  const memoizedTikTokEmbedComponents = useMemo(() => {
-    return videoData.slice(0, videosToShow).map((v, i) => {
-      containerRefs.current[i] = containerRefs.current[i] || React.createRef();
-      return (
-        <div key={i} ref={containerRefs.current[i]}>
-          {loadedVideos.includes(i) && (
-            <div key={i} ref={containerRefs.current[i]}>
-              {loadedVideos.includes(i) && (
-                <UseCaseCard
-                  videoId={v.videoId}
-                  category={v.category}
-                  title={v.title}
-                />
-              )}
-            </div>
-          )}
-        </div>
-      );
-    });
-  }, [videoData, loadedVideos, containerRefs, videosToShow]);
-
-  useEffect(() => {
-    const options = {
-      root: null,
-      rootMargin: "0px",
-      threshold: 0.5,
-    };
-
-    const handleIntersection = (index) => (entries) => {
-      entries.forEach((entry) => {
-        if (entry.isIntersecting && !loadedVideos.includes(index)) {
-          console.log(`Video at index ${index} is in view`);
-          if (loadedVideos.length <= videoData.length) {
-            setLoadedVideos((prev) => [...prev, index]);
-          }
-        }
-      });
-    };
-
-    containerRefs.current.forEach((containerRef, index) => {
-      const currentContainer = containerRef.current;
-
-      const observer = new IntersectionObserver(
-        handleIntersection(index),
-        options
-      );
-
-      if (currentContainer) {
-        observer.observe(currentContainer);
-      }
-
-      return () => {
-        if (currentContainer) {
-          observer.unobserve(currentContainer);
-        }
-      };
-    });
-  }, [containerRefs, loadedVideos, videoData.length]);
 
   const handleViewMore = () => {
     setVideosToShow((prev) => prev + 3);
@@ -75,7 +14,18 @@ const AllUsecases = ({ videoData }) => {
         <div className="flex flex-col gap-2">
           <div className="flex flex-col">
             <div className="px-6 grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-              {memoizedTikTokEmbedComponents}
+              {videoData.slice(0, videosToShow).map((v, i) => {
+                return (
+                  <div key={i}>
+                    <UseCaseCard
+                      key={i}
+                      videoId={v.videoId}
+                      category={v.category}
+                      title={v.title}
+                    />
+                  </div>
+                );
+              })}
             </div>
           </div>
         </div>
